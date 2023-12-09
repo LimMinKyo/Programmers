3425e6c811f0ea1d24c5710706078ab9eae9c89e
LimMinKyo <story1400@naver.com>
2023-12-09 14:58:59 +0900

    Solve 큰 수 만들기

---
+++ Level 2/큰 수 만들기/solution.js
@@ -0,0 +1,15 @@
+function solution(number, k) {
+  const stack = [];
+
+  for (let i = 0; i < number.length; i++) {
+    while (stack.at(-1) < number[i] && k > 0) {
+      stack.pop();
+      k--;
+    }
+    stack.push(number[i]);
+  }
+
+  stack.splice(number.length - k, k);
+
+  return stack.join("");
+}
