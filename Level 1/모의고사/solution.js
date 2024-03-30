7f6c791cdca2a2b93e29a0f559e98aa3064849f4
LimMinKyo <story1400@naver.com>
2024-03-30 12:11:18 +0900

    Solve 모의고사

--- Level 1/모의고사/solution.js
+++ Level 1/모의고사/solution.js
@@ -1,30 +1,29 @@
 function solution(answers) {
-  const scores = {
-    1: 0,
-    2: 0,
-    3: 0,
-  };
+  const patterns = [
+    [1, 2, 3, 4, 5],
+    [2, 1, 2, 3, 2, 4, 2, 5],
+    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+  ];
 
-  const mathHateMan1Pick = [1, 2, 3, 4, 5];
-  const mathHateMan2Pick = [2, 1, 2, 3, 2, 4, 2, 5];
-  const mathHateMan3Pick = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  const scores = new Array(3).fill(0);
 
-  answers.forEach((answer, index) => {
-    if (mathHateMan1Pick[index % mathHateMan1Pick.length] === answer) {
-      scores[1]++;
-    }
-    if (mathHateMan2Pick[index % mathHateMan2Pick.length] === answer) {
-      scores[2]++;
-    }
-    if (mathHateMan3Pick[index % mathHateMan3Pick.length] === answer) {
-      scores[3]++;
+  answers.forEach((answer, i) => {
+    patterns.forEach((pattern, j) => {
+      if (answer === pattern[i % pattern.length]) {
+        scores[j]++;
+      }
+    });
+  });
+
+  const maxScore = Math.max(...scores);
+
+  const highestScores = [];
+
+  scores.forEach((score, index) => {
+    if (score === maxScore) {
+      highestScores.push(index + 1);
     }
   });
 
-  const highScore = Math.max(...Object.values(scores));
-
-  return Object.entries(scores)
-    .filter(([man, score]) => score === highScore)
-    .map(([man]) => +man)
-    .sort((a, b) => a - b);
+  return highestScores;
 }
