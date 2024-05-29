af25e54993f30f397f7822d8071cbcee495d06e4
LinMinKyo <story1400@naver.com>
2024-05-29 15:11:17 +0900

    Update 메뉴 리뉴얼

--- Level 2/메뉴 리뉴얼/solution.py
+++ Level 2/메뉴 리뉴얼/solution.py
@@ -11,11 +11,12 @@ def solution(orders, courses):
         for order in orders:
             menus += combinations(sorted(order), course)
 
-        counter = Counter(menus)
+        most_ordered_menus = Counter(menus).most_common()
 
-        for menu, count in counter.items():
-            max_count = max(counter.values())
-            if max_count != 1 and count == max_count:
-                answer.append("".join(menu))
+        answer += [
+            "".join(menu)
+            for menu, count in most_ordered_menus
+            if count > 1 and count == most_ordered_menus[0][1]
+        ]
 
     return sorted(answer)
