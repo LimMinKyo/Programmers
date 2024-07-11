42c6afce3c3545a571e13ad2d284c2338a884c65
LinMinKyo <story1400@naver.com>
2024-07-11 09:18:07 +0900

    Solve 전화번호 목록

--- Level 2/전화번호 목록/solution.js
+++ Level 2/전화번호 목록/solution.js
@@ -1,8 +1,8 @@
 function solution(phone_book) {
   phone_book.sort();
 
-  const isPrefix = phone_book.some(
-    (phone, index) => phone_book[index + 1]?.indexOf(phone) === 0
+  const isPrefix = phone_book.some((phone, index) =>
+    phone_book[index + 1]?.startsWith(phone)
   );
 
   return !isPrefix;
