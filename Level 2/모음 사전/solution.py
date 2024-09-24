d7cbe2005eb96161df07b4cced8dacfad13a26b3
LinMinKyo <story1400@naver.com>
2024-09-24 10:23:23 +0900

    Solve 모음 사전

---
+++ Level 2/모음 사전/solution.py
@@ -0,0 +1,10 @@
+from itertools import product
+
+
+def solution(word):
+    return (
+        sorted(
+            "".join(p) for i in range(5) for p in product("AEIOU", repeat=i + 1)
+        ).index(word)
+        + 1
+    )
