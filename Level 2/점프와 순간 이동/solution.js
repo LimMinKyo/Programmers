d85a3a4f4a5e059b43fbe28b9dee4f84fa4ade4d
LinMinKyo <story1400@naver.com>
2025-03-20 11:14:14 +0900

    Solve 점프와 순간 이동

--- Level 2/점프와 순간 이동/solution.js
+++ Level 2/점프와 순간 이동/solution.js
@@ -1,14 +1,3 @@
 function solution(n) {
-  let answer = 0;
-  
-  while (n !== 0) {
-    if (n % 2 === 0) {
-      n /= 2;
-    } else {
-      n--;
-      answer++;
-    }
-  }
-  
-  return answer;
+  return n.toString(2).replaceAll("0", "").length;
 }
