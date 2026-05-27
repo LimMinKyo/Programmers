b8bc1156673577381d985dec4874d4f65191d0bf
LimMinKyo <story1400@naver.com>
2026-05-27 11:44:05 +0900

    Solve 순서쌍의 개수

---
+++ Level 0/코딩테스트 입문/순서쌍의 개수/solution.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+
+    for i in range(1, int(n**0.5) + 1):
+        if i * i == n:
+            answer += 1
+            continue
+
+        if n % i == 0:
+            answer += 2
+
+    return answer
