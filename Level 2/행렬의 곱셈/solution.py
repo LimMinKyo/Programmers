2862d4d0d699dc7ff7d2a5fd4a2cceb9c0839276
LimMinKyo <story1400@naver.com>
2026-06-30 10:03:48 +0900

    Solve 행렬의 곱셈

--- Level 2/행렬의 곱셈/solution.py
+++ Level 2/행렬의 곱셈/solution.py
@@ -1,3 +1,10 @@
+# import numpy as np
+#
+#
+# def solution(arr1, arr2):
+#     return (np.matrix(arr1) * np.matrix(arr2)).tolist()
+
+
 def solution(arr1, arr2):
     return [
         [sum(row * col for row, col in zip(row, col)) for col in zip(*arr2)]
